360764063fe2bcb25fa7c4b288b4ac889d040904
gbatislaong <gerrymae.roxasbatislaong@gmail.com>
2024-10-07 19:17:36 +0800

    Welcome pages

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import homeBgLogo from '../../assets/images/home-bg.png'
 import './style.scss'
 
 function HomePage() {
   return (
     <div className="home-page">
+      <img className="home-bg" src={homeBgLogo} alt="EDUQUEST" />
       <div className="content">
         <h1>
           <span>The</span>
-          <span>Lost Keys</span>
-          <span>of</span>
+          <span class="text-large">Lost Keys</span>
+          <span class="text-small">of</span>
           <span>Inventa Island</span>
         </h1>
-        <p>An Interactive educational quest that will test your Math, Reading, Science and History!</p>
-        <p>Age 8-12 years</p>
-        <Link to="/welcome">Start new</Link>
+        <p>An Interactive educational quest that will test your Math,<br/> Reading, Science and History!</p>
+        <p class="pt-md">Age 8-12 years</p>
+        <div class="btn-wrapper"><Link to="/welcome">Start new</Link></div>
+      
       </div>
     </div>
   );
--- src/pages/WelcomePage/index.js
+++ src/pages/WelcomePage/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './style.scss'
+import owl from '../../assets/images/owl-welcome.png'
+import './style.scss';
 
 function WelcomePage() {
   return (
     <div className="welcome-page">
       <div className="content">
+        <div class="img-wrapper">
+          <img className="owl" src={owl} alt="owl" />
+        </div>
         <h1>Welcome to Inventa Island</h1>
         <p>The land of Inventa Island was once nourished by the Crystal of Knowledge, a powerful artifact said to ensure harmony by nurturing wisdom and learning. Dr. Veilmind, blinded by his desire for control, unleashed a curse that</p>
-        <Link to="/game-map">Next</Link>
+        <div class="btn-wrapper"><Link to="/game-map" class="btn-style ">Next</Link></div>
       </div>
     </div>
   );
