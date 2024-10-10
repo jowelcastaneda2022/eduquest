1e185384f427c0120ec8028e1bcd8a4b0f54e93d
Jumar Ochinang <jochinang@cainc.com>
2024-10-10 14:55:52 +0800

    fix: word scramble page

--- src/pages/WordScramblePage/index.js
+++ src/pages/WordScramblePage/index.js
@@ -1,17 +1,41 @@
-import React, { useEffect } from 'react';
-import { PageLoader, WordScrambleGame, PageHeader } from '../../components';
-import { fetchWordScrambleData } from '../../mutations';
-import { ResultPage } from '../../pages';
-import './style.scss';
-import { store, updateStore } from '../../unistore'
+import React, { useState, useEffect } from 'react';
 import { connect } from 'unistore/react';
+import { PageLoader, WordScrambleGame, GameNav, GameModal } from '../../components';
+import { fetchWordScrambleData } from '../../mutations';
+import { getItem } from '../../helpers';
+import { store, updateStore } from '../../unistore';
+import './style.scss';
 
 function WordScramblePage(props) {
   const { gameData, gameFinished, finalScore } = props.wordScramble;
+  const [openInstructionModal, setOpenInstructionModal] = useState(false);
+  const [openResultModal, setOpenResultModal] = useState(false);
 
   useEffect(() => {
+    checkHistory();
+    toggleInstructionModal();
     fetchWordScrambleData();
   }, []);
+
+  const checkHistory = () => {
+    const { wordScramble } = store.getState();
+    updateStore({
+      wordScramble: {
+        ...wordScramble,
+        finalScore: getItem('wordScramble') ? getItem('wordScramble').finalScore : 0,
+        gameFinished: false,
+        scorePercentage: getItem('wordScramble') ? getItem('wordScramble').scorePercentage : 0,
+      }
+    })
+  }
+
+  const toggleInstructionModal = () => {
+    setOpenInstructionModal(!openInstructionModal);
+  };
+
+  const toggleResultModal = () => {
+    setOpenResultModal(!openResultModal);
+  };
 
   const handleFinish = (score) => {
     const { wordScramble } = store.getState();
@@ -22,6 +46,7 @@ function WordScramblePage(props) {
         gameFinished: true,
       }
     })
+    toggleResultModal();
   };
 
   const handleRetry = () => {
@@ -34,7 +59,18 @@ function WordScramblePage(props) {
       }
     })
     fetchWordScrambleData();
+    toggleResultModal();
   };
+
+  const getScorePercentage = (percent) => {
+    const { wordScramble } = store.getState();
+    updateStore({
+      wordScramble: {
+        ...wordScramble,
+        scorePercentage: percent,
+      }
+    })
+  }
 
   if (gameData.fetching) {
     return <PageLoader />;
@@ -46,24 +82,33 @@ function WordScramblePage(props) {
 
   return (
     <div className="word-scramble-page">
-      <PageHeader text="Scramble Word Game" />
-      {!gameFinished ? (
-        <WordScrambleGame wordsData={gameData.data} onFinish={handleFinish} />
-      ) : (
-        <ResultPage
-          score={finalScore}
-          totalQuestions={0}
-          totalRounds={gameData.data.length}
-          onRetry={handleRetry}
-          scoringMode="perRound"
+      {!gameFinished 
+      ? <WordScrambleGame wordsData={gameData.data} onFinish={handleFinish} />
+      : <GameModal
+            onButtonClick={toggleResultModal}
+            type="withRate"
+            title="Scramble Savannah"
+            buttonText="Back to map"
+            score={finalScore}
+            totalQuestions={0}
+            totalRounds={gameData.data.length}
+            scoringMode="perRound"
+            gameNumber="2"
+            onRetry={handleRetry}
+            getScorePercentage={getScorePercentage}
+          />
+      }
+
+      {openInstructionModal && (
+        <GameModal description={"Drag the letters into its corresponding position to spell it right. Beat the timer and make your way through the different levels."}
+          onButtonClick={toggleInstructionModal}
+          type="howTo"
+          title="Scramble Savannah"
+          header=""
         />
       )}
 
-      <ul className="circles">
-        {Array.from({ length: 10 }).map((_, index) => (
-          <li key={index}></li>
-        ))}
-      </ul>
+      <GameNav onButtonClick={toggleInstructionModal} />
     </div>
   );
 }
